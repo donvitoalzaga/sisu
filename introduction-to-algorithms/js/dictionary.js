56018af132d2612bd191076f7b23ec4437362714
Don Vito Alzaga <dev.dawn024@gmail.com>
2017-08-13 22:23:18 +0800

    Add count and clear function for dictionary

--- introduction-to-algorithms/js/dictionary.js
+++ introduction-to-algorithms/js/dictionary.js
@@ -1,11 +1,11 @@
-
-
 function Dictionary() {
 	this.datastore = new Array();
 	this.add = add;
 	this.find = find;
 	this.remove = remove;
-	this.show = show
+	this.show = show;
+	this.count = count;
+	this.clear = clear;
 }
 
 function add(key, value) {
@@ -21,17 +21,36 @@ function remove(key) {
 }
 
 function show() {
-	for (var key of Object.keys(this.datastore)) {
+	for (var key of Object.keys(this.datastore).sort()) {
 		console.log(key + ': ' + this.datastore[key])
+	}
+}
+
+function count() {
+	var counter = 0;
+	for (var key of Object.keys(this.datastore)) {
+		counter++;
+	}
+	return counter;
+}
+
+function clear() {
+	for (var key of Object.keys(this.datastore)) {
+		delete this.datastore[key]
 	}
 }
 
 var phonebook = new Dictionary();
 
-phonebook.add('Mike', '123');
+phonebook.add("Raymond","123");
 phonebook.add("David", "345");
 phonebook.add("Cynthia", "456");
-console.log("David's extension: " + phonebook.find("David"));
-phonebook.remove("David");
+phonebook.add("Mike", "723");
+phonebook.add("Jennifer", "987");
+phonebook.add("Danny", "012");
+phonebook.add("Jonathan", "666");
 
 phonebook.show();
+phonebook.clear();
+
+console.log("Number of entries: " + phonebook.count());
